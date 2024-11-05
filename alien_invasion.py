d562f926e0e5d229bd47853dcfac80eeed2f321f
Jeppe Udesen <jeppe.udesen@gmail.com>
2024-11-05 19:48:41 +0100

    added bullet collision and aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,8 +1,11 @@
 import sys
 import pygame
+from random import randint
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
+
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
     
@@ -25,15 +28,10 @@ class AlienInvasion:
         
         # Set background color.
         self.bg_color = (230, 230, 230)
-        
-    def run_game(self):
-        """Start the main loop for the game."""
-        while True:
-            self._check_events()
-            self.ship.update()
-            self._update_bullets()
-            self._update_screen()
-            self.clock.tick(60)
+
+        # Create a fleet of aliens.
+        self.aliens = pygame.sprite.Group()
+        self._create_fleet()
             
     def _fire_bullet(self):
         """Create a new bullet and add it to the bullets group."""
@@ -77,6 +75,52 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+
+    def _create_fleet(self):
+        """Create a fleet of aliens."""
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        current_x, current_y = alien_width, alien_height
+
+        # Create rows of aliens.
+        while current_y < (self.settings.screen_height - 3 * alien_height):
+            while current_x < (self.settings.screen_width - 2 * alien_width):
+                # Create an alien and place it in the row.
+                self._create_alien(current_x, current_y)
+                current_x += 1.2 * alien_width
+            # Finished a row; reset x value, and increment y value.
+            current_x = alien_width
+            current_y += 2 * alien_height
+    
+    def _create_alien(self, x_position, y_position):
+        """Create an alien and place it in the fleet."""
+        new_alien = Alien(self)
+        new_alien.x = x_position
+        new_alien.rect.x = x_position
+        new_alien.rect.y = y_position
+        self.aliens.add(new_alien)
+
+    def _check_fleet_edges(self):
+        """Respond appropriately if any aliens have reached an edge."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _check_aliens_shot(self):
+        """Respond to aliens that have been shot."""
+        collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, self.settings.bullet_destructible, True)
+
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleet's direction."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.alien_drop_speed
+        self.settings.fleet_direction *= -1
+
+    def _update_aliens(self):
+        """Update the positions of all aliens in the fleet."""
+        self._check_fleet_edges()
+        self.aliens.update()
                     
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
@@ -88,9 +132,27 @@ class AlienInvasion:
         
         # Redraw the ship.
         self.ship.blitme()
+
+        # Redraw the aliens.
+        self.aliens.draw(self.screen)
         
         # Make the most recently drawn screen visible.
         pygame.display.flip()
+
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            self._check_events()
+            self.ship.update()
+            self._update_bullets()
+            self._update_aliens()
+            self._check_aliens_shot()
+            if not self.aliens:
+                self.bullets.empty()
+                self.settings.alien_speed += 1
+                self._create_fleet()
+            self._update_screen()
+            self.clock.tick(60)
         
             
 if __name__ == '__main__':
--- settings.py
+++ settings.py
@@ -2,8 +2,8 @@ class Settings:
     """A class to store all settings for Alien Invasion."""
     
     def __init__(self):
-        self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_width = 600
+        self.screen_height = 400
         self.bg_color = (230, 230, 230)
         
         # Ship settings.
@@ -14,4 +14,10 @@ class Settings:
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
-        self.bullets_allowed = 20
+        self.bullets_allowed =  100
+        self.bullet_destructible = True
+        # Alien settings.
+        self.alien_speed = 1.0
+        self.alien_drop_speed = 10
+        # fleet_direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
